eae76ff49210b5ad1119182e02aab98b2bd4ce59
Furkan <furkanilaslan@gmail.com>
2025-06-20 16:33:48 +0300

    commit

--- dugun_sitem/settings.py
+++ dugun_sitem/settings.py
@@ -118,18 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(str(BASE_DIR), 'staticfiles')
-
-
-# Medya dosyaları ayarları
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(str(BASE_DIR), 'media')
-MEDIA_ROOT = str(BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
